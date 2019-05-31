0683f86d4cf9fa2f00090a09cd1a0416ea00218d
Jonathan Wang <jonathanwang@Jonathans-MacBook-Pro.local>
2019-05-31 12:08:33 -0400

    Basic components created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ReplaceRules from "./components/ReplaceRules";
+import TextArea from "./components/TextArea";
 
-function App() {
-  return null;
+class App extends React.Component {
+	render() {
+		return (
+			<div class="app-container">
+				<TextArea />
+				<ReplaceRules />
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/TextArea.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class TextArea extends React.Component {
+    render() {
+        return null;
+    }
+}
+
+export default TextArea;
