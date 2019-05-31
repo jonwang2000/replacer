0cc3ab996956bbe7291ba05a1885f05a121f41fd
jonwang2000 <37186169+jonwang2000@users.noreply.github.com>
2019-05-31 14:24:11 -0400

    Live matching text between input and output

--- src/components/TextZone.js
+++ src/components/TextZone.js
@@ -1,19 +1,33 @@
 import React from "react";
 
 class TextZone extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { value: "Hello" };
+
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(event) {
+		this.setState({ value: event.target.value });
+	}
+
 	render() {
 		return (
 			<div className="textzone-div">
-				<textarea id="input-zone" className="zone-area">
-					{" "}
-				</textarea>
+				<textarea
+					id="input-zone"
+					className="zone-area"
+					value={this.state.value}
+					onChange={this.handleChange}
+				/>
 				->
 				<textarea
 					readonly="readonly"
 					id="output-zone"
-					className="zone-area"
+                    className="zone-area"
+                    value = {this.state.value}
 				>
-					{" "}
 				</textarea>
 			</div>
 		);
